1a7565401f94485c62fae5e75df1d2f6996b11a9
vivekThoke <vivekthoke5@gmail.com>
2026-03-03 21:23:10 +0530

    Added inteface for JWT token, Password Hash, Tenanat and User repo

---
+++ SupportAI.Application/Interfaces/ITenantRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SupportAI.Domain.Entities;
+
+namespace SupportAI.Application.Interfaces
+{
+    public interface ITenantRepository
+    {
+        Task AddAsync(Tenant tenant);
+    }
+}
---
+++ SupportAI.Application/Interfaces/IUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SupportAI.Domain.Entities;
+
+namespace SupportAI.Application.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<User?> GetByEmailAsync(string email);
+        Task AddAsync(User user);
+    }
+}
