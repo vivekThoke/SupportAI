812c19f55cdf7d3a6258c5f03221bea12ca8b7b0
vivekThoke <vivekthoke5@gmail.com>
2026-03-03 20:51:22 +0530

    Added Base class entity and Tenant entity

---
+++ SupportAI.Domain/Common/BaseEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SupportAI.Domain.Common
+{
+    public abstract class BaseEntity
+    {
+        public Guid Id { get; private set; } = Guid.NewGuid();
+        public DateTime CreatedAt { get; private set; } = DateTime.UtcNow;
+    }
+}
---
+++ SupportAI.Domain/Entities/Tenant.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SupportAI.Domain.Common;
+
+namespace SupportAI.Domain.Entities
+{
+    public class Tenant : BaseEntity
+    {
+        public string Name { get; private set; } = default!;
+
+        private readonly List<User> users = new();
+        public IReadOnlyCollection<User> Users => users.AsReadOnly();
+
+        private Tenant() { }
+
+        public Tenant(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+                throw new ArgumentException("Tenant cannot be empty");
+
+            Name = name;
+        }
+    }
+}
