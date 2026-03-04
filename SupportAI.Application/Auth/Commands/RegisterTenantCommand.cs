5cf0cdcecb2e4983486f517b9964d7339a32e198
vivekThoke <vivekthoke5@gmail.com>
2026-03-04 09:13:24 +0530

    Added package MediatR for CORS and implemented Register Tenanat command

--- SupportAI.Application/Auth/Commands/RegisterTenantCommand.cs
+++ SupportAI.Application/Auth/Commands/RegisterTenantCommand.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MediatR;
+using SupportAI.Application.Auth.DTOs;
 
 namespace SupportAI.Application.Auth.Commands
 {
-    public record RegisterTenantCommand
-    {
-
-    }
+    public record RegisterTenantCommand(
+            string TenantName,
+            string Email,
+            string Password
+        ) : IRequest<AuthResponse>;
 }
