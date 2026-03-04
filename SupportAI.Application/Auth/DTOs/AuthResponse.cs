583d7a854f517f57713e193b38d435356cb7494c
vivekThoke <vivekthoke5@gmail.com>
2026-03-04 09:09:06 +0530

    Added DTO for Auth Response, Login Request and Register Tenanat Request

---
+++ SupportAI.Application/Auth/DTOs/AuthResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SupportAI.Application.Auth.DTOs
+{
+    public record AuthResponse
+    {
+        string Token = default!;
+    }
+}
---
+++ SupportAI.Application/Auth/DTOs/RegisterTenantRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SupportAI.Application.Auth.DTOs
+{
+    public record RegisterTenantRequest
+    {
+        string TenantName = default!;
+        string Email = default!;
+        string Password = default!;
+    }
+}
