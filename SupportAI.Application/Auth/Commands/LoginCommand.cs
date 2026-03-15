dbee82067510d87cad74a6b93f37065f975ef256
vivekThoke <vivekthoke5@gmail.com>
2026-03-15 20:13:35 +0530

    Added login command object for CQRS

---
+++ SupportAI.Application/Auth/Commands/LoginCommand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+using SupportAI.Application.Auth.DTOs;
+
+namespace SupportAI.Application.Auth.Commands
+{
+    public record LoginCommand(
+        string email,
+        string password
+        ) : IRequest<AuthResponse>;
+}
