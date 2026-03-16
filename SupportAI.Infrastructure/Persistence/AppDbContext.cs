82f2b386e7db80783a5cce7f32077025df55b13e
vivekThoke <vivekthoke5@gmail.com>
2026-03-16 20:50:34 +0530

    Added DB Conttext and configured the table for Tenant and User

---
+++ SupportAI.Infrastructure/Persistence/AppDbContext.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SupportAI.Domain.Entities;
+
+namespace SupportAI.Infrastructure.Persistence
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Tenant> Tenants => Set<Tenant>();
+        public DbSet<User> Users => Set<User>();
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Tenant>(entity =>
+            {
+                entity.HasKey(x => x.Id);
+
+                entity.Property(x => x.Name)
+                    .IsRequired()
+                    .HasMaxLength(200);
+            });
+
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.HasKey(x => x.Id);
+
+                entity.Property(x => x.Email)
+                    .IsRequired()
+                    .HasMaxLength(200);
+
+                entity.Property(x => x.PasswordHash)
+                    .IsRequired();
+
+                entity.Property(x => x.Role)
+                    .IsRequired();
+
+                entity.HasOne<Tenant>()
+                    .WithMany(t => t.Users)
+                    .HasForeignKey(u => u.TenantId);
+            });
+        }
+
+
+
+    }
+}
