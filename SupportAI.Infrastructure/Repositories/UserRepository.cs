cef7d4c3b1131ff8058ee1b4d7955590a3d8d8c4
vivekThoke <vivekthoke5@gmail.com>
2026-03-16 23:17:37 +0530

    Added User repository that will get user by email and add user to database

---
+++ SupportAI.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SupportAI.Application.Interfaces;
+using SupportAI.Domain.Entities;
+using SupportAI.Infrastructure.Persistence;
+
+namespace SupportAI.Infrastructure.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly AppDbContext dbContext;
+
+        public UserRepository(AppDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public async Task<User?> GetByEmailAsync(string email)
+        {
+            return await dbContext.Users.FirstOrDefaultAsync(x => x.Email == email);
+        }
+
+        public async Task AddAsync(User user)
+        {
+            await dbContext.Users.AddAsync(user);
+
+            await dbContext.SaveChangesAsync();
+        }
+    }
+}
