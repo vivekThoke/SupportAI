b1887a130148f5ab278ab4d5fe717f447d8e6aff
vivekThoke <vivekthoke5@gmail.com>
2026-03-16 23:05:31 +0530

    Added tenant repository that will add tenant async to database

---
+++ SupportAI.Infrastructure/Repositories/TenantRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SupportAI.Application.Interfaces;
+using SupportAI.Domain.Entities;
+using SupportAI.Infrastructure.Persistence;
+
+namespace SupportAI.Infrastructure.Repositories
+{
+    public class TenantRepository : ITenantRepository
+    {
+        private readonly AppDbContext dbContext;
+
+        public TenantRepository(AppDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public async Task AddAsync(Tenant tenant)
+        {
+            await dbContext.Tenants.AddAsync(tenant);
+
+            await dbContext.SaveChangesAsync();
+        }
+    }
+}
