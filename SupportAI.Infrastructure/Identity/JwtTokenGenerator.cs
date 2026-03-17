f20dfe5b799f05010ffce70c033823f9d4a226fd
vivekThoke <vivekthoke5@gmail.com>
2026-03-17 09:28:16 +0530

    Updating the JWT token generation class

---
+++ SupportAI.Infrastructure/Identity/JwtTokenGenerator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.IdentityModel.Tokens;
+using SupportAI.Application.Interfaces;
+using SupportAI.Domain.Entities;
+
+namespace SupportAI.Infrastructure.Identity
+{
+    public class JwtTokenGenerator : IJwtTokenGenerator
+    {
+        private readonly string secretKey = "SUPER_SECRET_KEY_!@#";
+
+        public string GenerateToken(User user)
+        {
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey));
+
+            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]
+            {
+                    new Claim(JwtRegisteredClaimNames.Sub, user.Id.ToString()),
+                    new Claim("tenantId", user.TenantId.ToString()),
+                    new Claim(ClaimTypes.Role, user.Role.ToString())
+            };
+        }
+
+
+    }
+}
