1c758b2302b6b12311a8892f6ea53514266e3530
vivekThoke <vivekthoke5@gmail.com>
2026-03-17 09:10:24 +0530

    Implemneted pass word hash and verify with hashed password with password

---
+++ SupportAI.Infrastructure/Security/PasswordHasher.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SupportAI.Application.Interfaces;
+
+namespace SupportAI.Infrastructure.Security
+{
+    public class PasswordHasher : IPasswordHasher
+    {
+        public string Hash(string password)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(password);
+        }
+
+        public bool Verify(string password, string passWordHash)
+        {
+            return BCrypt.Net.BCrypt.Verify(password, passWordHash);
+        }
+    }
+}
