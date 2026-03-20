084c2047f7485c94d7bef855ad53980362f71a6e
vivekThoke <vivekthoke5@gmail.com>
2026-03-20 09:20:29 +0530

    Added Document model for storing, chuking and converting it to embeeded

---
+++ SupportAI.Domain/Entities/Document.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SupportAI.Domain.Common;
+
+namespace SupportAI.Domain.Entities
+{
+    public class Document : BaseEntity
+    {
+        public Guid TenantId { get; private set; }
+        public string FileName { get; private set; }
+        public string FilePath { get; private set; }
+        public string Status { get; private set; }
+
+        private Document() { }
+
+        public Document(Guid tenantId, string fileName, string filePath)
+        {
+            TenantId = tenantId;
+            FileName = fileName;
+            FilePath = filePath;
+        }
+
+        public void MarkReady() => Status = "Ready";
+        public void MarkFailed() => Status = "Failed";
+    }
+}
