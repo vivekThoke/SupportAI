3bc5ebbb5320a076617f1085e28499d83814a2f5
vivekThoke <vivekthoke5@gmail.com>
2026-03-21 11:06:33 +0530

    Added Text extractor from file based on pfd or text document

---
+++ SupportAI.Infrastructure/Processing/TextExtractor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UglyToad.PdfPig;
+
+namespace SupportAI.Infrastructure.Processing
+{
+    public class TextExtractor
+    {
+        public string Extract(string filePath)
+        {
+            if (filePath.EndsWith(".pdf"))
+            {
+                using var document = PdfDocument.Open(filePath);
+                var text = "";
+
+                foreach(var page in document.GetPages())
+                {
+                    text += page.Text;
+                }
+
+                return text;
+            }
+
+            return File.ReadAllText(filePath);
+        }
+    }
+}
