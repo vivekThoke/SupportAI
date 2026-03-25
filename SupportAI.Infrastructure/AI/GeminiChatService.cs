eb3a181c941a65e9163a42c793b491b0d14acfb6
vivekThoke <vivekthoke5@gmail.com>
2026-03-25 20:03:54 +0530

    Added gemini service to answer the specific question within context

---
+++ SupportAI.Infrastructure/AI/GeminiChatService.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using SupportAI.Application.Interfaces;
+
+namespace SupportAI.Infrastructure.AI
+{
+    public class GeminiChatService : IChatService
+    {
+        private readonly HttpClient _httpClient;
+        private readonly string _apiKey;
+
+        public GeminiChatService(HttpClient httpClient, IConfiguration configuration)
+        {
+            _httpClient = httpClient;
+            _apiKey = configuration["Gemini:ApiKey"];
+        } 
+        
+        public async Task<string> AskAsync(string question, List<string> context)
+        {
+            var combinedContext = string.Join("\n\n", context);
+
+            var prompt = $@"
+                            You are customer support AI.
+                            Answer only using the provided context.
+
+                            If answer is not in context, say:
+                            I don't know based on provided information.
+
+                            Context: {combinedContext}
+
+                            Question: {question}";
+
+            var request = new
+            {
+                contents = new[]
+                {
+                    new
+                    {
+                        parts = new[]
+                        {
+                            new { text = prompt }
+                        }
+                    }
+                }
+            };
+
+            var response = await _httpClient.PostAsJsonAsync(
+                                            $"https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key={_apiKey}",
+                                            request);
+
+            var json = await response.Content.ReadAsStringAsync();
+
+            if (!response.IsSuccessStatusCode)
+                throw new Exception(json);
+
+            var doc = JsonDocument.Parse(json);
+
+            var answer = doc.RootElement
+                .GetProperty("candidates")[0]
+                .GetProperty("content")
+                .GetProperty("parts")[0]
+                .GetProperty("text")
+                .GetString();
+
+            return answer;
+        }
+    }
+}
