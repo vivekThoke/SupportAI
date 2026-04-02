50e5d298b00c8e0753c283672c95d47075e03f07
vivekThoke <vivekthoke5@gmail.com>
2026-04-02 09:21:21 +0530

    added index protected route for login, register and chat page

--- SupportAI-Frontend/src/routes/index.tsx
+++ SupportAI-Frontend/src/routes/index.tsx
@@ -1,17 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { Loginpage } from '../pages/Loginpage'
 import ChatPage from '../pages/ChatPage'
 import RegisterPage from '../pages/RegisterPage'
+import { GuestRoute, ProtectedRoute } from './RouteGuards'
 
 const AppRoutes = () => {
+    const isAuthenticated = !!localStorage.getItem("token");
+    // const [isAuthenticated, setIsAuthenticated] = useState(false);
+
   return (
     <BrowserRouter>
         <Routes>
+            <Route element={<GuestRoute isAuthenticated={isAuthenticated}/>}>
+                <Route path='/' element={<Loginpage />}/>
+                <Route path='/register' element={<RegisterPage />}/>
+            </Route>
+
+            <Route element={<ProtectedRoute isAuthenticated={isAuthenticated}/>}>
+                <Route path='/chat' element={<ChatPage />}/>
+            </Route>
+{/* 
             <Route path='/' element={<Loginpage />}/>
             <Route path='/chat' element={<ChatPage />}/>
-            <Route path='/register' element={<RegisterPage />}/>
-            <Route />
+            <Route path='/register' element={<RegisterPage />}/> */}
+            <Route path='*' element={<p>Page not Found</p>}/>
         </Routes>
     </BrowserRouter>
   )
