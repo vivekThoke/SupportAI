b8739e84f409c0d07180d06c1c7414e22aa8141a
vivekThoke <vivekthoke5@gmail.com>
2026-04-03 20:18:38 +0530

    Making dockerized and production ready

--- SupportAI-Frontend/src/routes/index.tsx
+++ SupportAI-Frontend/src/routes/index.tsx
@@ -6,24 +6,29 @@ import RegisterPage from '../pages/RegisterPage'
 import { GuestRoute, ProtectedRoute } from './RouteGuards'
 
 const AppRoutes = () => {
-    const isAuthenticated = !!localStorage.getItem("token");
+    // const isAuthenticated = !!localStorage.getItem("token");
     // const [isAuthenticated, setIsAuthenticated] = useState(false);
 
   return (
     <BrowserRouter>
         <Routes>
-            <Route element={<GuestRoute isAuthenticated={isAuthenticated}/>}>
+            {/* <Route element={<GuestRoute isAuthenticated={isAuthenticated}/>}>
                 <Route path='/' element={<Loginpage />}/>
                 <Route path='/register' element={<RegisterPage />}/>
+
+                <Route path='/chat' element={<ChatPage />}/>
             </Route>
 
             <Route element={<ProtectedRoute isAuthenticated={isAuthenticated}/>}>
                 <Route path='/chat' element={<ChatPage />}/>
-            </Route>
-{/* 
+
+                <Route path='/' element={<Loginpage />}/>
+                <Route path='/register' element={<RegisterPage />}/>
+            </Route> */}
+
             <Route path='/' element={<Loginpage />}/>
             <Route path='/chat' element={<ChatPage />}/>
-            <Route path='/register' element={<RegisterPage />}/> */}
+            <Route path='/register' element={<RegisterPage />}/>
             <Route path='*' element={<p>Page not Found</p>}/>
         </Routes>
     </BrowserRouter>
