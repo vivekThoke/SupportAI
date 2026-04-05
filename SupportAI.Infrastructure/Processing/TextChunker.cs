5693fc1575a0f0096514d5899869f6422992b69a
vivekThoke <vivekthoke5@gmail.com>
2026-04-05 18:43:36 +0530

    In text chunker based on the sentences ending

--- SupportAI.Infrastructure/Processing/TextChunker.cs
+++ SupportAI.Infrastructure/Processing/TextChunker.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UglyToad.PdfPig.DocumentLayoutAnalysis;
 
 namespace SupportAI.Infrastructure.Processing
 {
@@ -17,6 +18,23 @@ namespace SupportAI.Infrastructure.Processing
                 chunks.Add(text.Substring(i, Math.Min(size, text.Length - i)));
             }
 
+            return chunks;
+        }
+
+        public List<string> Chunk(string text)
+        {
+            const int chunkSize = 500;
+            const int overleap = 100;
+
+            var chunks = new List<string>();
+
+            for (int i = 0; i < text.Length; i += (chunkSize - overleap))
+            {
+                var length = Math.Min(chunkSize, text.Length - i);
+                var chunk = text.Substring(i, length);
+
+                chunks.Add(chunk);
+            }
             return chunks;
         }
     }
